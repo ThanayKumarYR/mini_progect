f9ac28df7de58adb03007b92ec27e735be78fc15
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-08-16 21:24:12 +0530

    main commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/food.jpg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [img, setimg] = useState(logo);
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setimg(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="box">
+        <div id="card">
+          <div id="left">
+            <img src={img} alt="logo" />
+          </div>
+          <div id="right">
+            <div id="top">
+              <input
+                type="file"
+                name="image-upload"
+                id="input"
+                accept="image/*"
+                onChange={imageHandler}
+              />
+              <label htmlFor="input" className="image-upload">
+                Upload Image
+              </label>
+            </div>
+            <div id="bottom">
+              <pre>
+                <h3>Food           : ???</h3>
+                <h3>Name           : ???</h3>
+                <h3>Calories       : ???</h3>
+                <h3>Carbohydrates  : ???</h3>
+                <h3>Proteins       : ???</h3>
+                <h3>Fats           : ???</h3>
+                <h3>Vitamins       : ???</h3>
+                <h3>Dietary Fibres : ???</h3>
+                <h3>Minerals       : ???</h3>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
